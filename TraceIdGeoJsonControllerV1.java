2780e82bc8fba770bc8ce2b23c1cf96fcf4e5db3
Riley Li <71857976+MadaniKK@users.noreply.github.com>
2023-07-13 11:31:43 +0800

    Update TraceIdGeoJsonControllerV1.java
    merge the three request into one, handle the request based on different conditions

--- TraceIdGeoJsonControllerV1.java
+++ TraceIdGeoJsonControllerV1.java
@@ -16,59 +16,92 @@ public class TraceIdGeoJsonControllerV1 {
     }
 
 
-    @GetMapping("/byTraceIds")
-    public ResponseEntity getGeoJsonData(@RequestParam(value = "traceIds") List<String> traceIds) {
+//    @GetMapping("/byTraceIds")
+//    public ResponseEntity getGeoJsonDataByTraceIds(@RequestParam(value = "traceIds") List<String> traceIds) {
+//        /**
+//         Get GeoJson Data by TraceIds
+//         @Parameters:
+//         - traceIds: a list of traceIds
+//         @Returns:
+//         responseEntity; Body: a list of geoJson
+//        */
+//        ResponseEntity responseEntity = geoJsonServiceV1.getGeoJsonDataByTraceIds(traceIds);
+//
+//        return responseEntity;
+//    }
+//
+//    @GetMapping("/byTraceIdsAndTimeRange")
+//    public ResponseEntity getGeoJsonDataWithinTimeRange(@RequestParam("traceIds") List<String> traceIds,
+//                                                        @RequestParam("startTime") Long startTime,
+//                                                        @RequestParam("endTime") Long endTime) {
+//        /**
+//         Get GeoJson Data by TraceIds Within TimeRange, returning trace whose start_timestamp within the time range
+//         @Parameters:
+//         - traceIds: a list of traceIds
+//         - timeRange: [startTime, endTime], time are Strings in ISO 8601 format
+//         @Returns:
+//         responseEntity; Body: a list of geoJson
+//        */
+//
+//        ResponseEntity responseEntity = geoJsonServiceV1.getGeoJsonDataWithinTimeRange(traceIds, startTime, endTime);
+//
+//        return responseEntity;
+//    }
+
+//    @GetMapping("/byTraceIdsAndBbox")
+//    public ResponseEntity getGeoJsonDataWithinBbox(@RequestParam("traceIds") List<String> traceIds,
+//                                                   @RequestParam("minLon") float minLon,
+//                                                   @RequestParam("minLat") float minLat,
+//                                                   @RequestParam("maxLon") float maxLon,
+//                                                   @RequestParam("maxLat") float maxLat) {
+//        /**
+//         get GeoJson Data by TraceIds Within bbox
+//         @Parameters:
+//         - traceIds: a list of traceIds
+//         - minLon
+//         - minLat
+//         - maxLon
+//         - maxLat
+//         @Returns:
+//         responseEntity; Body: a list of geoJson
+//        */
+//
+//        ResponseEntity responseEntity = geoJsonServiceV1.getGeoJsonDataWithinBbox(traceIds ,minLon, minLat, maxLon, maxLat);
+//
+//        return responseEntity;
+//    }
+    @GetMapping("/getGeoJson")
+    public ResponseEntity getGeoJsonData(@RequestParam(value = "traceIds") List<String> traceIds,
+                                          @RequestParam(value = "startTime", required = false) Long startTime,
+                                          @RequestParam(value = "endTime", required = false) Long endTime,
+                                          @RequestParam(value = "minLon", required = false) Float minLon,
+                                          @RequestParam(value = "minLat", required = false) Float minLat,
+                                          @RequestParam(value = "maxLon", required = false) Float maxLon,
+                                          @RequestParam(value = "maxLat", required = false) Float maxLat) {
         /**
-         Get GeoJson Data by TraceIds
+         Get GeoJson Data by TraceIds either based on startTime, EndTime or BBox
+         When time param is Null, it is given a default time
          @Parameters:
-         - traceIds: a list of traceIds
-         @Returns:
-         responseEntity; Body: a list of geoJson
-        */
-        ResponseEntity responseEntity = geoJsonServiceV1.getGeoJsonData(traceIds);
-
-        return responseEntity;
-    }
-
-    @GetMapping("/byTraceIdsAndTimeRange")
-    public ResponseEntity getGeoJsonDataWithinTimeRange(@RequestParam("traceIds") List<String> traceIds,
-                                                        @RequestParam("startTime") Long startTime,
-                                                        @RequestParam("endTime") Long endTime) {
-        /**
-         Get GeoJson Data by TraceIds Within TimeRange, returning trace whose start_timestamp within the time range
-         @Parameters:
-         - traceIds: a list of traceIds
-         - timeRange: [startTime, endTime], time are Strings in ISO 8601 format
-         @Returns:
-         responseEntity; Body: a list of geoJson
-        */
-
-        ResponseEntity responseEntity = geoJsonServiceV1.getGeoJsonDataWithinTimeRange(traceIds, startTime, endTime);
-
-        return responseEntity;
-    }
-
-    @GetMapping("/byTraceIdsAndBbox")
-    public ResponseEntity getGeoJsonDataWithinBbox(@RequestParam("traceIds") List<String> traceIds,
-                                                   @RequestParam("minLon") float minLon,
-                                                   @RequestParam("minLat") float minLat,
-                                                   @RequestParam("maxLon") float maxLon,
-                                                   @RequestParam("maxLat") float maxLat) {
-        /**
-         get GeoJson Data by TraceIds Within bbox
-         @Parameters:
-         - traceIds: a list of traceIds
+         - traceIds
+         - startTime
+         - endTime
          - minLon
          - minLat
          - maxLon
          - maxLat
          @Returns:
-         responseEntity; Body: a list of geoJson
-        */
+         responseEntity: Body is a list of geoJsonData [{geoJson, properties:{traceId, startTime, EndTime}},...,{geoJson, properties:{traceId, startTime, EndTime}}]
+         */
 
-        ResponseEntity responseEntity = geoJsonServiceV1.getGeoJsonDataWithinBbox(traceIds ,minLon, minLat, maxLon, maxLat);
-
+        ResponseEntity responseEntity;
+        if (minLon != null && minLat != null && maxLon != null && maxLat != null){
+            responseEntity = geoJsonServiceV1.getGeoJsonDataWithinBbox(traceIds, minLon, minLat, maxLon, maxLat, startTime, endTime);
+        }else{
+            responseEntity = geoJsonServiceV1.getGeoJsonDataWithinTimeRange(traceIds, startTime, endTime);
+        }
         return responseEntity;
+
+
     }
 
-}
+    }
